dceed05642585d42f9b858cb8015261839fb7aa5
varchasvipandey <varchasvip@gmail.com>
2024-02-25 12:31:53 +0530

    feat: menu loader

--- app/(user)/(private)/_components/side-nav-drawer/side-nav-drawer.tsx
+++ app/(user)/(private)/_components/side-nav-drawer/side-nav-drawer.tsx
@@ -4,16 +4,23 @@ import { Button } from "@/components/ui/button";
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import { SideNav } from "../";
 import { getIcon } from "@/components/icons";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { usePathname } from "next/navigation";
 
 const MenuIcon = getIcon("menu");
 
 export const SideNavDrawer = () => {
+  const pathname = usePathname();
+
   const [isOpen, setIsOpen] = useState(false);
 
   const handleOnOpenChange = (isOpen: boolean) => setIsOpen(isOpen);
 
   const closeMenu = () => setIsOpen(false);
+
+  useEffect(() => {
+    closeMenu();
+  }, [pathname]);
 
   return (
     <Sheet open={isOpen} onOpenChange={handleOnOpenChange}>
@@ -24,7 +31,7 @@ export const SideNavDrawer = () => {
       </SheetTrigger>
       <SheetContent side="left" className="xs:w-[240px] w-[320px]">
         <div className="mt-4">
-          <SideNav onItemClick={closeMenu} />
+          <SideNav />
         </div>
       </SheetContent>
     </Sheet>
--- app/(user)/(private)/_components/side-nav/side-nav.tsx
+++ app/(user)/(private)/_components/side-nav/side-nav.tsx
@@ -3,6 +3,7 @@
 import { getIcon } from "@/components/icons";
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
+import { Skeleton } from "@/components/ui/skeleton";
 import { Routes, getOrgRoute } from "@/lib/routes";
 import { cn } from "@/lib/utils";
 import { useOrganization } from "@clerk/nextjs";
@@ -14,47 +15,43 @@ const SettingsIcon = getIcon("settings");
 const BillingIcon = getIcon("billing");
 const IsActiveIcon = getIcon("circle");
 
-interface Props {
-  onItemClick?: () => void;
-}
-
-export const SideNav = ({ onItemClick }: Props) => {
+export const SideNav = () => {
   const pathname = usePathname();
   const router = useRouter();
-  const { organization } = useOrganization();
+  const { organization, isLoaded } = useOrganization();
 
   const orgId = organization?.id;
 
   const navItems = orgId ? getNavRoutes(orgId) : [];
 
-  const handleOnClickItem = (href: string) => {
-    router.push(href);
-    if (onItemClick) onItemClick();
-  };
+  const handleOnClickItem = (href: string) => router.push(href);
 
   return (
-    <div className="flex flex-col space-y-2 items-start">
-      {navItems.map((item) =>
-        item.type === "route" ? (
-          <Button
-            key={item.href}
-            variant="nav"
-            onClick={() => handleOnClickItem(item.href || "")}
-            className={cn("w-full justify-start", pathname === item.href && "bg-primary/10 text-primary font-normal")}
-            aria-label={item.label}
-          >
-            <span className="w-full flex items-center justify-between">
-              <span className="flex">
-                {item.icon}
-                {item.label}
+    <div className={cn("flex flex-col items-start", isLoaded ? "space-y-2" : "space-y-4")}>
+      {!isLoaded && Array.from({ length: navItems.length - 1 }).map((_, i) => <Skeleton key={i} className="h-10 w-full bg-primary/10" />)}
+
+      {isLoaded &&
+        navItems.map((item) =>
+          item.type === "route" ? (
+            <Button
+              key={item.href}
+              variant="nav"
+              onClick={() => handleOnClickItem(item.href || "")}
+              className={cn("w-full justify-start", pathname === item.href && "bg-primary/10 text-primary font-normal")}
+              aria-label={item.label}
+            >
+              <span className="w-full flex items-center justify-between">
+                <span className="flex">
+                  {item.icon}
+                  {item.label}
+                </span>
+                {pathname === item.href && <IsActiveIcon size={10} className="anim-fade-in fill-primary stroke-transparent" />}
               </span>
-              {pathname === item.href && <IsActiveIcon size={10} className="anim-fade-in fill-primary stroke-transparent" />}
-            </span>
-          </Button>
-        ) : (
-          <Separator key={item.id} className="w-2/3 mx-auto" />
-        ),
-      )}
+            </Button>
+          ) : (
+            <Separator key={item.id} className="w-2/3 mx-auto" />
+          ),
+        )}
     </div>
   );
 };
